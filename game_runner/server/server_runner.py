8ff91fe90e6541373fec850fc1b218ea3d663b8e
Parham Kazemi <pkazemi3@gmail.com>
2018-11-20 19:37:56 +0330

    Fixed server arguments

--- game_runner/server/server_runner.py
+++ game_runner/server/server_runner.py
@@ -9,9 +9,9 @@ SERVER_FILE_NAME = 'UIAIServer2018_1.1.x86_64'
 
 
 def start(team1_name, team2_name, log_file_name=datetime.now().isoformat()):
-    # --mode=cvc --logfile=log_file_name.log --speed=20 --team1name=chom --team2name=choooom -nographic -batchmode
+    # --mode=cvc --logfile=log_file_name.log --speed=20 --team1name=chom --team2name=choooom -nographics -batchmode
     command = ['./{}'.format(SERVER_FILE_NAME),
-               '--mode=cvc', '--logfile={}.log'.format(log_file_name), '--speed=20', '-nographic', '-batchmode',
+               '--mode=cvc', '--logfile={}.log'.format(log_file_name), '--speed=20', '-nographics', '-batchmode',
                ' --team1name={}'.format(team1_name), '--team2name={}'.format(team2_name)]
     process = subprocess.Popen(command, cwd=SERVER_DIR_PATH)
     return process
