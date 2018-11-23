82e8ce4cffd7e78d19f2c17bc093459fcfa31c8c
Parham Kazemi <pkazemi3@gmail.com>
2018-11-23 14:56:43 +0330

    Fixed server speed

--- game_runner/server/server_runner.py
+++ game_runner/server/server_runner.py
@@ -1,8 +1,5 @@
 import os
-import threading
 import subprocess
-
-from game_runner.run_game import return_results
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,7 +10,7 @@ SERVER_FILE_NAME = 'UIAIServer2018_1.1.x86_64'
 def start(game_id, team1_name, team2_name):
     # --mode=cvc --logfile=log_file_name.log --speed=20 --team1name=chom --team2name=choooom -nographics -batchmode
     command = ['./{}'.format(SERVER_FILE_NAME),
-               '--mode=cvc', '--logfile={}.log'.format(str(game_id)), '--speed=100', '-nographics', '-batchmode',
+               '--mode=cvc', '--logfile={}.log'.format(str(game_id)), '--speed=5', '-nographics', '-batchmode',
                '--team1name={}'.format(team1_name), '--team2name={}'.format(team2_name)]
     process = subprocess.Popen(command, cwd=SERVER_DIR_PATH)
     return process
