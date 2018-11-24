2198d81487aecb692c9aac21f59df7672606ff6f
Parham Kazemi <pkazemi3@gmail.com>
2018-11-24 13:04:45 +0330

    Updated update script

--- update.sh
+++ update.sh
@@ -4,4 +4,7 @@ git pull
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
+sudo rm -rf /var/lib/lxc/uiai2018base/rootfs/opt/uiai2018/game_runner
 sudo cp -vr game_runner /var/lib/lxc/uiai2018base/rootfs/opt/uiai2018/
+sudo service uwsgi restart
+sudo service nginx restart
