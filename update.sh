33853de8571544b49e7f8742c1a0c9e34737ba23
Parham Kazemi <pkazemi3@gmail.com>
2018-12-01 22:15:34 +0330

    Updated script to use lxc base2

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ git pull
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-sudo rm -rf /var/lib/lxc/uiai2018base/rootfs/opt/uiai2018/game_runner
-sudo cp -vr game_runner /var/lib/lxc/uiai2018base/rootfs/opt/uiai2018/
+sudo rm -rf /var/lib/lxc/base2/rootfs/opt/uiai2018/game_runner
+sudo cp -vr game_runner /var/lib/lxc/base2/rootfs/opt/uiai2018/
 sudo rm -rf /opt/uiai2018/game_runner
 sudo cp -vr game_runner /opt/uiai2018/
 sudo service uwsgi restart
