c47b661dd5ac281a226853a29c21ca7027cce8c1
Parham Kazemi <pkazemi3@gmail.com>
2018-12-01 22:30:38 +0330

    Increased game speed

--- game_runner/server/server_runner.py
+++ game_runner/server/server_runner.py
@@ -10,7 +10,7 @@ SERVER_FILE_NAME = 'UIAI2018Server_Linux_x86_64_3.0.x86_64'
 def start(game_id, team1_name, team2_name):
     # --mode=cvc --logfile=log_file_name.log --speed=20 --team1name=chom --team2name=choooom -nographics -batchmode
     command = ['./{}'.format(SERVER_FILE_NAME),
-               '--mode=cvc', '--logfile={}.log'.format(str(game_id)), '--speed=10',
+               '--mode=cvc', '--logfile={}.log'.format(str(game_id)), '--speed=20',
                '--team1name={}'.format(team1_name), '--team2name={}'.format(team2_name)]
     process = subprocess.Popen(command, cwd=SERVER_DIR_PATH)
     return process
